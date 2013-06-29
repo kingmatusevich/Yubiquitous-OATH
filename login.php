edede6fec4861a8a0fb23dad5321c311e68c81dd
Javier Matusevich <javiermatusevich@gmail.com>
2013-06-29 19:23:24 -0300

    Added login.php file for obtaining current secret database

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+  import_once('Yubikey.php');
+  $otp = $_GET['otp'];
+  $obj;
+  substr($otp, 0, 12);
+  sha1(substr);
+  $mysqli = new mysqli("localhost", "my_user", "my_password", "world");
+  if ($mysqli->connect_errno)
+  {
+    printf('-1');
+    exit();
+  }
+  /* Select queries return a resultset */
+  if ($result = $mysqli->query("SELECT Data from Secrets WHERE ID='".mysqli_sanitize_input))
+  {
+    if ($result->num_rows != 1)
+    {
+      printf('0');
+      exit();
+    } else
+    {
+      $obj = $result->fetch_object();
+      /* free result set */
+      $result->close();
+      $token = new Yubikey('id');
+      $token->setCurlTimeout(20);
+      $token->setTimestampTolerance(1000);
+      if ($token->verify($otp))
+      {
+        printf($data);
+      }
+      else printf('0');
+    }
+  }
+  $mysqli->close();
+ ?>
